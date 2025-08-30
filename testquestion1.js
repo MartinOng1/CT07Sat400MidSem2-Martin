86bcf456a7c64cbbfb8d5cef5233913a52407a25
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-08-30 08:33:24 +0000

    Aug 30, 2025, 8:33 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,14 @@
+function setup () {
+    new Canvas(600, 400);
+    world.gravity.y = 10;
+}
 
+function draw() {
+    background(240);
+    if (mouse.presses()) {
+        let circle = new Sprite(mouse.x, mouse.y, 30, 30);
+        circle.diameter = 30;
+    }
+    noStroke();
+    fill(20);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,24 @@
+function setup () {
+    new Canvas(600, 400);
+    world.gravity.y = 10;
+
+    floor = new Sprite();
+    floor.x = 300;
+    floor.y = 400;
+    floor.width = 600;
+    floor.height = 30;
+    floor.collider = 'static';
+    floor.colour = "cyan";
+}
+
+function draw() {
+    background(240);
+    if (mouse.presses()) {
+        let circle = new Sprite(mouse.x, mouse.y, 30, 30);
+        circle.diameter = 30;
+        circle.bounciness = 0.5;
+    }
+    noStroke();
+    fill(20);
+}
 
